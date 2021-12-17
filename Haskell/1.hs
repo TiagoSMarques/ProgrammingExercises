932647a77254e3bf50e77352bb4262beeb9ad28d
TiagoSMarques <marques.tiago19@gmail.com>
2021-12-17 00:00:15 +0000

    h_2

--- Haskell/1.hs
+++ Haskell/1.hs
@@ -5,10 +5,10 @@ factorial n = if n == 0 then 1 else n * factorial (n - 1)
 inR min max x = x >= min && x <= max
 
 main = do
-  putStrLn factorial 5
+-- putStrLn factorial 5
 
---   putStrLn "What is 5! ?"
---   x <- readLn
---   if x == factorial 5
---     then putStrLn "You're right!"
---     else putStrLn "You're wrong!"
+  putStrLn "What is 5! ?"
+  x <- readLn
+  if x == factorial 5
+    then putStrLn "You're right!"
+    else putStrLn "You're wrong!"
