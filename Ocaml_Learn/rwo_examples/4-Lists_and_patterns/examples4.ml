31d57cc9d37d8a9e4277b05eec6861d330a9d788
TiagoSMarques <marques.tiago19@gmail.com>
2023-07-06 13:07:55 +0100

    render table exercise

--- Ocaml_Learn/rwo_examples/4-Lists_and_patterns/examples4.ml
+++ Ocaml_Learn/rwo_examples/4-Lists_and_patterns/examples4.ml
@@ -10,11 +10,11 @@ let rec find_first_repeat list =
   | x :: y :: tl -> if x = y then Some x else find_first_repeat (y :: tl)
 
 (* The |> opetator can be used swap the placement of function and argument allowing to chain functions without naming each return *)
-let path = "/usr/bin:/usr/local/bin:/bin:/sbin:/usr/bin";;
+let path = "/usr/bin:/usr/local/bin:/bin:/sbin:/usr/bin"
 
-String.split ~on:':' path
-|> List.dedup_and_sort ~compare:String.compare
-|> List.iter ~f:print_endline
+(* String.split ~on:':' path
+   |> List.dedup_and_sort ~compare:String.compare
+   |> List.iter ~f:print_endline *)
 
 (* Function to rmove num from the array *)
 let rec dropValue l to_drop =
@@ -24,7 +24,7 @@ let rec dropValue l to_drop =
       let newTail = dropValue tl to_drop in
       if hd = to_drop then newTail else hd :: newTail
 
-let () = List.iter ~f:(printf "%d ") (dropValue [ 1; 2; 3; 3; 4; 3; 5; 9 ] 3)
+(* let () = List.iter ~f:(printf "%d ") (dropValue [ 1; 2; 3; 3; 4; 3; 5; 9 ] 3) *)
 
 (* Function render_table that, given a list of column headers and a list of rows, prints them out in a well-formatted text table. *)
 
@@ -37,7 +37,7 @@ let t_rows =
     [ "ML"; "Robin Milner"; "1973" ];
     [ "OCaml"; "Xavier Leroy"; "1996" ];
   ]
-(*
+(* Example output:
    | language | architect      | first release |
    |----------+----------------+---------------|
    | Lisp     | John McCarthy  | 1958          |
@@ -46,31 +46,35 @@ let t_rows =
    | OCaml    | Xavier Leroy   | 1996          |
 *)
 
-(* function to compute the maximum width of each column of data *)
-
+(* Compute the maximum width of each column of data *)
 let max_widths header rows =
   let lengths l = List.map ~f:String.length l in
   List.fold rows ~init:(lengths header) ~f:(fun acc row ->
       List.map2_exn ~f:Int.max acc (lengths row))
 
-(* Create the line that separates the heder from the rows *)
-(* Create strings of dashes based on the max size for the col *)
-
-let widths = max_widths t_headers t_rows
-
+(* Create strings of dashes that separates the header from the rows based on the max size for the col *)
 let buildSep width =
   let n_dashes = List.map ~f:(fun l -> String.make l '-') width in
   "|-" ^ String.concat n_dashes ~sep:"-+-" ^ "-|"
 
+(* Add space to fill to the desired width *)
 let padStr txt_in desired_width =
   txt_in ^ String.make (desired_width - String.length txt_in) ' '
 
+(* Format the row to a string *)
 let render_row row widths_list =
-  List.map2_exn row widths_list ~f:(fun item w -> "| " ^ padStr item w ^ " |")
+  let row_list =
+    List.map2_exn row widths_list ~f:(fun item w -> padStr item w)
+  in
+  "| " ^ String.concat row_list ~sep:" | " ^ " |\n"
+
+(* Print the table *)
+let print_table header rows =
+  let widths = max_widths header rows in
+  String.concat
+    (render_row header widths :: buildSep widths :: "\n"
+    :: List.map t_rows ~f:(fun r -> render_row r widths))
+  |> print_string
 ;;
 
-printf "seprator :%s \n" (buildSep widths)
-(*
-   let rec buildSep n_dash =
-     let sep = "" in
-     match n_dash with [] -> "" | hd :: tl -> sep ^ hd *)
+print_table t_headers t_rows
